0654b2ae68a696dc170ad4dc41ca4e806e1dc758
BALA9605 <balasakthi9605@gmail.com>
2025-09-17 12:47:08 +0530

    Frontend,backend created successfully

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+// Sample route
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
